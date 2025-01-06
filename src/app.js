58260f208da82d7d6268e1d2bcfcbcdd5a10a3f2
Manveer Kaur <manveerkaur1664@gmail.com>
2025-01-06 11:51:39 +0530

    regEx and dynamic route

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ app.listen(3000, ()=> {
     console.log("Server is successfully running on port 3000");
 });
 
+app.get("/user/:userID/:password", (req,res) => {
+    console.log(req.params)
+    res.send(`hello from user, userId ${req.params.userID}, password ${req.params.password}`);
+});
+
 app.get("/user", (req,res) => {
     res.send({"firstName": "Manveer", "lastName": "Kaur"});
 });
@@ -16,6 +21,18 @@ app.post("/user", (req,res) => {
 app.delete("/user", (req,res) => {
     console.log("Delete data from the database");
     res.send("Data deleted successfully.");
+});
+
+app.get("/ab?c", (req,res) => {
+    res.send("hello from abc or ac");
+});
+
+app.get("/de+f", (req,res) => {
+    res.send("hello from def or deef or deeeeeef etc.");
+});
+
+app.get("/xy*z", (req,res) => {
+    res.send("hello from xyAnythingz");
 });
 
 app.use("/hello", (req,res) => {
