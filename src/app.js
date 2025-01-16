a20249abc899dc09b42a676b61edae6cd296782a
Manveer Kaur <manveerkaur1664@gmail.com>
2025-01-16 11:39:51 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
+app.post("/signup", async (req, res) => {
+    const user = new User({
+        firstName: 'Manveer',
+        lastName: 'Kaur',
+        emailId: 'mk@gmail.com',
+        password: 'mk@09',
+        age: 26,
+        gender: 'Female'
+    })
+    try {
+        await user.save();
+        res.send("User added successfully");
+    } catch(err) {
+        res.status(400).send("Error saving the user" + err.message);
+    }
+   
+})
 connectDB()
 .then(() => {
     console.log("Database connection established ....");
