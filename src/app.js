4998e5a86ddffdc670cc16cb81893b880955c310
manveerKaurB <manveerkaur1664@gmail.com>
2025-03-12 10:36:15 +0530

    login api and use bcrypt library to validate password

--- src/app.js
+++ src/app.js
@@ -20,6 +20,27 @@ app.post("/signup", async (req, res) => {
     }
 })
 
+app.post("/login", async (req, res) => {
+    try {
+        const {emailId, password} = req.body;
+        // validation of data
+        const user = await User.findOne({emailId});
+        if(!user) {
+            throw new Error("Invalid credentials");
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(isPasswordValid){
+            res.send("User logged in successfully");
+        }
+        else {
+            throw new Error("Invalid credentials");
+        }
+
+    } catch(err) {
+        res.status(400).send("Error: " + err.message);
+    }
+})
+
 // fetch user by email
 app.get("/user", async (req, res) => {
     try{
