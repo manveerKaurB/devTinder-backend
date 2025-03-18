06fc5beb087140199f9dbeae5eeab7549f28d136
manveerKaurB <manveerkaur1664@gmail.com>
2025-03-18 10:28:04 +0530

    review request api

--- src/routes/request.js
+++ src/routes/request.js
@@ -43,4 +43,40 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
     }
 })
 
+requestRouter.post("/request/review/:status/:requestId", userAuth, async (req, res) => {
+    try{
+        const loggedInUser = req.user; // attached by userAuth
+        const {status, requestId} = req.params;
+        const allowedStatus = ["accepted", "rejected"];
+        if(!allowedStatus.includes(status)) {
+           return res.status(400).send("Invalid status type: " + status);
+        }
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId: loggedInUser._id,
+            status: 'interested'
+        });
+        if(!connectionRequest) {
+            return res.status(404).send({message: "Connection request not found!"});
+        }
+        connectionRequest.status = status;
+        const data = await connectionRequest.save();
+        res.json({
+            message: "connection request " + status,
+            data
+        })
+        // komal => ankita
+        // then logged in user should be ankita, as ankita would  be the one who is reviewing the request
+        // so toUserId should be the loggedInUserId
+        // if status is interested, then only receiver can accept/reject it. if its ignored there no way to accept/reject it.
+        // loggedInId === toUserId
+        // status === interested
+        // requestId should be valid
+
+    }
+    catch(err) {
+        res.status(400).send("Error: " + err.message);
+    }
+})
+
 module.exports = requestRouter;
