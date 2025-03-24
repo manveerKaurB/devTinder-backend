d79dec1d77a625f423c1fed40e3c8e06461406b8
manveerKaurB <manveerkaur1664@gmail.com>
2025-03-24 09:54:13 +0530

    signup api changes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -13,8 +13,14 @@ authRouter.post("/signup", async (req, res) => {
         // encryption of password
         const passwordHash = await bcrypt.hash(password, 10);
         const user = new User({firstName, lastName, emailId, password: passwordHash});
-        await user.save();
-        res.send("User added successfully");
+        const savedUser = await user.save();
+        // send token in cookie to have a valid session
+        const token = await savedUser.getJWT();
+        res.cookie("token", token, {expires: new Date(Date.now() + 8 * 3600000)});
+        res.json({
+            message: "User created successfully",
+            data: savedUser
+        });
     } catch(err) {
         res.status(400).send("Error saving the user" + err.message);
     }
